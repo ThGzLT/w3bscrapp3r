41e477616de5f8d409622649bf3a4baffa4a94f1
ThGzLT <Thgzlt@gmail.com>
2018-11-17 18:16:34 +0200

    first

---
+++ manoscrapperis/abc.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Web Scrapper</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+a {
+    font-style: normal;
+    color: white;
+}
+</style>
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row content">
+        <form action="scrapping.php">
+            <div class="input-group input-group-sm">
+                <input type="text" class="form-control" name="q" placeholder="Search here....">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">Download Content</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
